98f66f7e9a1a2eaca6df6f44e5860ef750c3faf9
Liva Rakotoarimino <lrakotoarimino@ingenosya.mg>
2017-06-07 11:56:50 +0300

    Update settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -250,9 +250,9 @@ $databases = array (
   array (
     'default' => 
     array (
-      'database' => 'siit_bi_2_copie',
-      'username' => 'siit',
-      'password' => 'Djkhq4d54gk',
+      'database' => '',
+      'username' => '',
+      'password' => '',
       'host' => 'localhost',
       'port' => '',
       'driver' => 'mysql',
